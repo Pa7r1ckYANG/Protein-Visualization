18be81532bf8d67d265d8305c19815541ed825d4
Pa7r1ckYANG <56457008+Pa7r1ckYANG@users.noreply.github.com>
2024-05-01 12:20:36 -0400

    Add files via upload

---
+++ protein_visualize.py
@@ -0,0 +1,11 @@
+import matplotlib as mp
+from Bio import SeqIO  # For reading and writing sequence files
+from Bio.Seq import Seq  # For working with sequences
+from Bio.PDB import PDBParser  # For working with protein data files
+import py3Dmol
+import tkinter
+
+class parse_PDB()
+    pass
+
+class 
